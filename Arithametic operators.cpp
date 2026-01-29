edfd6726536cd6018d6ef81a0854ab03b3607278
unknown <sakshideshmukh737@gmail.com>
2026-01-30 04:38:56 +0530

    added arithmetic operators

---
+++ Arithametic operators.cpp
@@ -0,0 +1,15 @@
+//Arithmetic Operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20, b = 10;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Modulus: " << a % b << endl;
+
+    return 0;
+}
