e163c7c830189773d92976a2a8bfdac035aea190
unknown <sakshideshmukh737@gmail.com>
2026-01-30 07:49:42 +0530

    Relational Operators.cpp

---
+++ Relational Operators.cpp
@@ -0,0 +1,14 @@
+//Relational Operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 10;
+
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+
+    return 0;
+}
