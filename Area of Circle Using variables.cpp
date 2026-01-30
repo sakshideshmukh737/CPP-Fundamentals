40b0eadd534c54bd69253b6ef42e63e664f6300d
unknown <sakshideshmukh737@gmail.com>
2026-01-30 05:51:57 +0530

    Area of circle using variables

---
+++ Area of Circle Using variables.cpp
@@ -0,0 +1,12 @@
+//Area of Circle Using Variables
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radius, area;
+    radius = 7;
+    area = 3.14 * radius * radius;
+
+    cout << "Area = " << area;
+    return 0;
+}
---
+++ Assignment Operators.cpp
@@ -0,0 +1,15 @@
+//Assignment Operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    a += 5;
+    cout << "a += 5: " << a << endl;
+
+    a -= 3;
+    cout << "a -= 3: " << a << endl;
+
+    return 0;
+}
