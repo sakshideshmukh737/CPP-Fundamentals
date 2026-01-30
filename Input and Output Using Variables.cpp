83f0d33ddedb8bf015791190aefb7d6ca1dffedc
unknown <sakshideshmukh737@gmail.com>
2026-01-30 08:04:25 +0530

    Input and Output Using Variables .cpp

---
+++ Input and Output Using Variables.cpp
@@ -0,0 +1,15 @@
+//Input and Output Using Variables
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
+}
