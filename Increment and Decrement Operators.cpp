e53107ab77dff13a101a648533ea6dea6d099bfa
unknown <sakshideshmukh737@gmail.com>
2026-01-30 08:11:48 +0530

    Increment and Decrement Operators.cpp

---
+++ Increment and Decrement Operators.cpp
@@ -0,0 +1,13 @@
+//Increment and Decrement Operators
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+
+    cout << "Pre-increment: " << ++a << endl;
+    cout << "Post-decrement: " << a-- << endl;
+    cout << "Final value: " << a << endl;
+
+    return 0;
+}
