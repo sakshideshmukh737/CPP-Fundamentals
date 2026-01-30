ed52a076b70d79590853b3a58e420d8363a6d81b
unknown <sakshideshmukh737@gmail.com>
2026-01-30 09:08:08 +0530

    CharacterDataType.cpp FloatingPointDataType.cpp

---
+++ CharacterDataType.cpp
@@ -0,0 +1,10 @@
+Character Data Type (char)
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'A';
+
+    cout << "Grade = " << grade;
+    return 0;
+}
---
+++ FloatingPointDataType.cpp
@@ -0,0 +1,12 @@
+Floating Point Data Type (float and double)
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14f;
+    double radius = 5.5;
+
+    double area = pi * radius * radius;
+    cout << "Area of circle = " << area;
+    return 0;
+}
