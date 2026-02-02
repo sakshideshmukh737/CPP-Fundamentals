785f9af6e2efa0075477a8b800f3b121188b9b7d
unknown <sakshideshmukh737@gmail.com>
2026-02-02 16:27:00 +0530

    Reverse an Array.cpp Store and Print Array Elements.cpp

---
+++ Reverse an Array.cpp
@@ -0,0 +1,13 @@
+Reverse an Array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 4; i >= 0; i--) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
