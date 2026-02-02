757d5d85d1542f611605b66029fd5e3f9a5eaad6
unknown <sakshideshmukh737@gmail.com>
2026-02-02 16:29:31 +0530

    Sum of Array Elements.cpp

---
+++ Sum of Array Elements.cpp
@@ -0,0 +1,15 @@
+Sum of Array Elements
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += a[i];
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
