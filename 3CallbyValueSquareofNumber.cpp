979e575bfd2ef460d6b90f6db47ff55e61a1e2de
unknown <sakshideshmukh737@gmail.com>
2026-02-11 12:37:00 +0530

    Improved comments

--- 3CallbyValueSquareofNumber.cpp
+++ 3CallbyValueSquareofNumber.cpp
@@ -1,15 +1,19 @@
+/* 
+This program calculates the square of a number using call by value.
+The function works on a copy of the variable, so the original value of 'a' is not changed.
+ */
 #include <iostream>
 using namespace std;
 
 // Function returns square of number
 int square(int x) {
-    // Uses local copy of x
+// Uses local copy of x
     return x * x;       
 }
 int main() {
     int a = 4;
 
-    // Value is passed, result is returned
+// Value is passed, result is returned
     cout << square(a);
 
     return 0;
